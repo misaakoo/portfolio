3b6bf60107709a3d9a1f324c6aeb6d49e31faedf
misaakoo <107912224+misaakoo@users.noreply.github.com>
2022-07-04 17:20:48 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// slider option
+
+$(document).ready(function(){
+   $('.slider').bxSlider({
+      auto: true,
+      mode: 'fade',
+      speed: 1000,
+      pager: true,
+      captions: true
+   });
+});
